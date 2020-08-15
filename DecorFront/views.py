e2bef6241f037d2b6369716d1bc411fdbcb64c36
jakesh-bohaju <jakesh.bohaju@gmail.com>
2020-08-15 20:17:55 +0545

    worled form and gallery

--- DecorFront/views.py
+++ DecorFront/views.py
@@ -40,7 +40,7 @@ class BaseView(View):
         'navbar_pages': Page.objects.filter(navbar=True),
         'categories': Category.objects.order_by('title').all(),
         'services': Services.objects.filter(status=True),
-        'product': Product.objects.all(),
+        'product': Product.objects.order_by('?').all(),
         'contact': ContactUs.objects.order_by('-id'),
         'profile': Profile.objects.order_by('-id'),
         'message': suchan
